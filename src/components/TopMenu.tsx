181c723750e5fbc0ae3cad62db1ac5d212f167da
point50 <pones0050@gmail.com>
2026-03-17 11:48:50 +0700

    add next-auth

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image"
 import TopMenuItem from "./TopMenuItem";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Link from "next/link";
 
-export default function TopMenu () {
+export default async function TopMenu () {
+    const session = await getServerSession(authOptions)
+
     return (
         <div className="h-[50px] bg-white fixed top-0 left-0 right-0 z-30 border-t border-b border-gray-300 flex flex-row-reverse items-center">
 
@@ -16,7 +21,10 @@ export default function TopMenu () {
 
             <TopMenuItem title="Booking" pageRef="/booking" />
             <TopMenuItem title="Select Venue" pageRef="/venue" />
-
+            
+            {
+                session? <Link href = "api/auth/signout"> <div className = "flex center absolute left-0 h-full px-2 text-cyan-600 text-sm">Sign-Out of {session.user?.name} </div></Link> : <Link href = "api/auth/signin"><div className = "flex center absolute left-0 h-full px-2 text-cyan-600 text-sm">Sign-In</div></Link>
+            }
         </div>
     );
 }
